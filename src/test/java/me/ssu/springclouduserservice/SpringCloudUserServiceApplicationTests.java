20a61a33d6b339aa785eebe2ea2c7252c304add7
김수환 <zzanggoon8@hiconsy.com>
2022-10-28 14:50:02 +0900

    first commit

---
+++ src/test/java/me/ssu/springclouduserservice/SpringCloudUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ssu.springclouduserservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
